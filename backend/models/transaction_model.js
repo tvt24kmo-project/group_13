618b593f260f5e5ccc1b3ad46b43d5ad8b3f9c27
Mihail Konstantinov <Konstantinovm1986@gmail.com>
2025-01-24 09:21:50 +0200

    transaction model and route

---
+++ backend/models/transaction_model.js
@@ -0,0 +1,37 @@
+const db=require('../database');
+
+const transaction = {
+    getAll:function(callback) {
+        return db.query('SELECT * FROM transaction', callback);
+    },
+
+    getById:function(id,callback){
+        return db.query('SELECT * FROM transaction WHERE id_transaction=?',[id],callback);
+    },
+
+   
+
+    add:function(transaction_data,callback){
+        return db.query('INSERT INTO transaction (transaction_type, sum, date, type,id_account) VALUES(?,?,?,?,?)',
+            [transaction_data.transaction_type,
+                transaction_data.sum,
+                transaction_data.date,
+                transaction_data.type,
+            transaction_data.id_account],callback);   
+    },
+
+    delete:function(id,callback){
+        return db.query('DELETE FROM transaction WHERE id_transaction=?',[id],callback);
+    },
+
+    update:function(id,transaction_data,callback){
+        return db.query('UPDATE transaction SET transaction_type=?, sum=?,date=?, type=?,id_account=? WHERE id_transaction=?',
+            [transaction_data.transaction_type,
+                transaction_data.sum,
+                transaction_data.date,
+                transaction_data.type,
+                transaction_data.id_account,id],callback);
+    }
+};
+
+module.exports=transaction;
