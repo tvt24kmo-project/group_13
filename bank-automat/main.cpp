6dd8a5dc49dcb1596b45ed393bcf39662398120c
Sara Vehviläinen <sara@vehvilainen.cc>
2025-02-10 21:32:04 +0200

    Frontendin koodista korjattu ongelmat REST kutsujen kanssa, korjattu myös että näkymät näyttävät asiat oikein.

--- bank-automat/main.cpp
+++ bank-automat/main.cpp
@@ -1,11 +1,10 @@
-#include "mainwindow.h"
+#include "mainwindow.h" // Sisällytetään mainwindow.h-tiedosto, joka sisältää MainWindow-luokan määrittelyn.
+#include <QApplication> // Sisällytetään QApplication-luokka, joka on Qt:n sovelluksen ydinluokka ja tarvitaan GUI-sovelluksissa.
 
-#include <QApplication>
-
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) // Määritellään pääohjelma, joka on sovelluksen aloituspiste. Se ottaa komentoriviparametrit vastaan.
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QApplication a(argc, argv); // Luodaan QApplication-olio, joka hallitsee sovelluksen tapahtumasilmukkaa ja muita perusasioita.
+    MainWindow w; // Luodaan MainWindow-luokan olio, joka edustaa pääikkunaa.
+    w.show(); // Näytetään pääikkuna ruudulla.
+    return a.exec(); // Käynnistetään Qt:n tapahtumasilmukka, joka odottaa ja käsittelee käyttäjän syötteitä.
 }
